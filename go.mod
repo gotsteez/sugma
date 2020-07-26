a3b49ca669f901ea546422dd707cda7a7a9f4b52
zMrKrabz <anhardy381@gmail.com>
2020-07-25 20:56:20 -0400

    Configured UTLS to work on google'

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/zMrKrabz/cf-reverse-proxy
 go 1.14
 
 require (
-	github.com/go-chi/chi v4.1.2+incompatible // indirect
-	github.com/go-chi/cors v1.1.1 // indirect
-	github.com/refraction-networking/utls v0.0.0-20200723202749-c9b914be2861 // indirect
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/go-chi/cors v1.1.1
+	github.com/refraction-networking/utls v0.0.0-20200723202749-c9b914be2861
 	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381
 )
--- main.go
+++ main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"log"
+	"net"
 	"net/http"
-	"net/http/httputil"
 	"net/url"
 
+	"net/http/httputil"
+
+	// "net/url"
+
+	"fmt"
+
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/cors"
+	utls "github.com/refraction-networking/utls"
+	"golang.org/x/net/http2"
 )
 
-func main() {}
+func main() {
+	r := router()
+
+	log.Println("Starting http server")
+	http.ListenAndServe(":8080", r)
+}
 
 func router() *chi.Mux {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
 	r.Use(cors.Handler(cors.Options{
 		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
@@ -26,9 +42,25 @@ func router() *chi.Mux {
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	}))
 
-	r.Get("/", CfBypass)
+	r.Get("/", test)
+	r.Get("/cf", CfBypass)
 
 	return r
+}
+
+func test(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	rawurl := query.Get("url")
+	redirect, err := url.Parse(rawurl)
+
+	if err != nil || rawurl == "" {
+		log.Println("Error ocurred while parsing url ", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	requestHostName := redirect.Host
+	w.Write([]byte("Requested url is " + requestHostName))
 }
 
 // CfBypass is handles your request, changes tls fingerprint, useragent, etc, and bypasses cf
@@ -44,7 +76,84 @@ func CfBypass(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	r.Header.Add("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0")
-	proxy := httputil.NewSingleHostReverseProxy(redirect)
-	proxy.ServeHTTP(w, r)
+	roller, err := utls.NewRoller()
+	if err != nil {
+		log.Panicln("Error while creating new roller", err)
+		w.WriteHeader(400)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	requestHostName := redirect.Host
+
+	log.Println("Dialing", requestHostName)
+	conn, err := roller.Dial("tcp", requestHostName+":443", requestHostName)
+
+	if err != nil {
+		log.Println("Error occurred while dialing the server", err)
+		w.WriteHeader(400)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	resp, err := httpGetOverConn(conn, conn.HandshakeState.ServerHello.AlpnProtocol, redirect)
+
+	defer resp.Body.Close()
+
+	if err != nil {
+		w.Write([]byte("Error after httpGetOverConn " + err.Error()))
+		return
+	}
+
+	body, err := httputil.DumpResponse(resp, true)
+
+	if err != nil {
+		w.Write([]byte("Error while dumping request body " + err.Error()))
+		return
+	}
+
+	w.Write(body)
+}
+
+func httpGetOverConn(conn net.Conn, alpn string, reqURL *url.URL) (*http.Response, error) {
+	requestHostName := reqURL.Hostname()
+	req := &http.Request{
+		Method: "GET",
+		URL:    &url.URL{Host: requestHostName + " /"},
+		Header: make(http.Header),
+		Host:   requestHostName,
+	}
+
+	switch alpn {
+	case "h2":
+		req.Proto = "HTTP/2.0"
+		req.ProtoMajor = 2
+		req.ProtoMinor = 0
+
+		tr := http2.Transport{}
+		cConn, err := tr.NewClientConn(conn)
+
+		defer tr.CloseIdleConnections()
+
+		if err != nil {
+			return nil, err
+		}
+
+		return cConn.RoundTrip(req)
+	case "http/1,1":
+		req.Proto = "HTTP/1.1"
+		req.ProtoMajor = 1
+		req.ProtoMinor = 1
+
+		err := req.Write(conn)
+
+		if err != nil {
+			return nil, err
+		}
+
+		return http.ReadResponse(bufio.NewReader(conn), req)
+
+	default:
+		return nil, fmt.Errorf("unsupported ALPN: %v", alpn)
+	}
 }
